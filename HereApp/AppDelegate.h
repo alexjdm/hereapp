b5922eafbe087b2ed1c245e1938316c8ed1a3563
Alex Diaz <adiaz@geovictoria.com>
2017-01-06 22:58:56 -0300

    Initial Commit

---
+++ HereApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HereApp
+//
+//  Created by Alex Diaz on 1/6/17.
+//  Copyright © 2017 Alex Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HereApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HereApp
+//
+//  Created by Alex Diaz on 1/6/17.
+//  Copyright © 2017 Alex Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
